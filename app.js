fc2f25cbe7fd19a94f21f73ea941f48cc88f9f72
sambhavgupta0705 <sambhavgupta0705@gmail.com>
2022-09-26 08:29:04 +0530

    adding files

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 const express=require('express')
-
+const mongoose =require('mongoose')
 const app =express();
 
-//routes
-app.get('/',(req,res)=>{
-    res.send('We are at home tab')
-})
-app.get('/posts',(req,res)=>{
-    res.send('We are Posts');
+const User=require("./model/user")
+require('dotenv/config')
 
+app.use(express.json()); 
+//routes
+
+// const customMiddleware=(re,res,next)=>{
+//     console.log('Welcome to My MiddleWare')
+//     next();
+// }
+// app.use(customMiddleware);
+app.get('/',(req,res)=>{
+    res.send("First Request!!!")
 })
-app.listen(5050);
+
+app.get("/users",(req,res)=>{
+    let users=["Sambhav","Superguy","flutter guy"];
+
+    // res.send(users)
+    res.send({
+        users:users,
+    })
+});
+
+
+app.post("/create_users",async(req,res)=>{
+try{
+    const myuser=new User(req.body);
+    // console.log(req.body.name);
+    await myuser.save()
+    res.send(myuser)
+}catch(err){
+    res.send({message:err})
+}
+});
+
+
+mongoose.connect(
+    process.env.DB_CONNECTION_STRING,
+    (req,res)=>{
+    console.log("Connected to the databse")
+});
+
+app.listen(5050,()=>{
+    console.log('Listening to 5050');
+});
